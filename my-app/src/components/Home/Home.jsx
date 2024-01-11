38877fe1e1f8dd6fded3ddb313c51767a5ec2ad6
bhavya <bhavya.gupta@citiustech.com>
2024-01-11 19:17:15 +0530

    added css changes

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,13 +1,28 @@
-import { useNavigate } from 'react-router-dom';
+import './Home.css';
+const Home = (props) => {
 
-const Home = (props)=>{
-     
-    const {loggedIn , email} = props
-    const navigate= useNavigate()
-
-    const OnButtonClick=()=>{        
+    const { loggedIn, email } = props
+    
+    const OnButtonClick = () => {
     }
-    return 
+    return <div className='mainContainer'>
+        <div className={'titleContainer'}>
+            <div><h1>Welcome!</h1></div>
+        </div>
+        <div><h3>This Is The Home Page.</h3></div>
+        <div className={'buttonContainer'}>
+            <input className={"button button1"}
+                type="button" onClick={OnButtonClick}
+                value={loggedIn ? "Log Out" : "Log In"} />
+                <div className={'buttonContainer'}>
+            <input className={"button button2"}
+                type="button" onClick={OnButtonClick}
+                value={loggedIn ? "Log Out" : "Register User"} />
+            {(loggedIn ? <div>
+                Your email adress is {email}</div> : <div />)}
+        </div>
+        </div>
+    </div>
 }
 
 export default Home;
